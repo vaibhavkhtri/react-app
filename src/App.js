968e30930e6fbb851e0a0fb1ad18b99a299045b4
vaibhavkhtri <vaibhav.maini@bytecodetechnologies.in>
2024-05-28 13:51:34 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Hello world.
+          Edit <code>src/App.js</code> Yo bro.
         </p>
         <a
           className="App-link"
